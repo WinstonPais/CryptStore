42c61fdd50146015e2a8390295f12ce9c74b4f89
Shane Misquith <shane.misquith@gmail.com>
2022-06-25 23:55:21 +0530

    Fixed background image for carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import "./Home.css";
 import { Carousel, Card } from 'antd';
-import Carousel1 from "../images/carousel1.png";
 import Carousel2 from "../images/carousel2.png";
-import Carousel3 from "../images/carousel3.png";
 import Comics from "../images/comics.png";
 import ArtemisFowl from "../images/ArtemisFowl.png";
 import MobyDick from "../images/MobyDick.png";
@@ -14,7 +12,7 @@ import Dictionaries from "../images/dictionaries.png";
 import Fantasy from "../images/fantasy.png";
 import Horror from "../images/horror.png";
 
-const carousel = [Carousel1,Carousel2,Carousel3];
+const carousel = [Carousel2];
 const catCard = [Adventure, Fantasy,Horror,Dictionaries];
 const Home = () => {
 
